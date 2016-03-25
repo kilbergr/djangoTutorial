183105d54ed973802d77ef75af0645c092b7bcff
Rebecca Kilberg <rekilberg@gmail.com>
2016-03-25 16:23:16 -0700

    better admin settings

--- polls/models.py
+++ polls/models.py
@@ -12,8 +12,12 @@ class Question(models.Model):
         return self.question_text
         
     def was_published_recently(self):
-        return timezone.now() - datetime.timedelta(days=1) <= self.pub_date <= timezone.now()
-
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+    
 @python_2_unicode_compatible
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
